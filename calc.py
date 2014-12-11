2d9ee7b3c80d8706fbb33324159d84f828614d42
Jordan Holland <jordieph@gmail.com>
2014-12-11 16:15:55 +1100

    Add divide

--- calc.py
+++ calc.py
@@ -7,6 +7,10 @@ def add_all(nums):
 def multiply_all(nums):
 	return reduce(lambda a, b: a * b, nums)
 
+def divide_all(nums):
+	return reduce(lambda a, b: a / b, nums)
+
+
 if __name__ == '__main__':
 	command = sys.argv[1]
 	nums = map(float, sys.argv[2:])
@@ -18,6 +22,8 @@ if __name__ == '__main__':
         	print(min(nums))
 	elif command == 'max':
 		print(max(nums))	
+	elif command == 'divide':
+		print(divide_all(nums))
 	else:
 		usage = "calc.py [add|multiply] NUM1 [NUM2 [NUM3 [...]]]"
 		print(usage)
